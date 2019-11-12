4ef354050ca88a2482e4d456b91991719d04a7f9
eli livshitz <eli.livshitz@gmail.com>
2019-11-12 16:37:12 +0200

    + examples how to browserify libx.js

---
+++ examples/browserify.js
@@ -0,0 +1,63 @@
+/*
+How to run:
+- `node examples/browserify.js`
+	To compile any TS files and browserify main.js and put compiled version in /examples/build/*.
+	Open /examples/build/index.html to view, access and play around with libx.js in the browser.
+- `$ node examples/browserify.js --watch --serve`
+	Do the above and also watch for main.js, *.ts files and also serve the index.html file via local server/
+- `$ node examples/browserify.js --watch --serve --minify`
+	Do the above but also minify main.js file (slower).
+*/
+
+const libx = require('libx.js');
+libx.pax = require("../src/index"); //pax.libx.js
+libx.node = require("libx.js/node");
+
+(async ()=>{ /* init */
+	var src = __dirname + "/src";
+	var dest = __dirname + "/build";
+	var mainJS = src + '/main.js';
+
+	var shouldWatch = libx.node.args.watch || false;
+	var shouldMinify = libx.node.args.minify || false;
+	var shouldServe = libx.node.args.serve || false;
+	libx.pax.config.debug = true;
+
+	await libx.pax.delete(dest);
+
+	var bundlerOptions = {
+		tsify: true,
+		// target: { node: 'v6.16.0' },
+		babelifyOptions: {
+			// global: true,
+			// sourceMaps: false
+		}
+	};
+
+	var p1 = libx.pax.copy(mainJS, dest, ()=>[
+		libx.pax.middlewares.browserify(bundlerOptions),
+		libx.pax.middlewares.if(shouldMinify, libx.pax.middlewares.minify()),
+	], shouldWatch, {
+		callback: ()=> console.log('build done')
+	});
+
+	var p2 = libx.pax.copy(src + '/index.html', dest, null, shouldWatch, { debug: false });
+
+	if (shouldServe) {
+		var port = 3010
+		libx.log.info(`test: serving... http://0.0.0.0:${port}/main.js`);
+		libx.pax.serve(dest, { port: port }, [dest + '/**/*.*']);
+	}
+
+	await Promise.all([p1, p2, ]);
+
+	if (shouldWatch) {
+		libx.pax.watchSimple([src + '/**/*.ts'], (ev, p)=>{
+			if (ev.type != 'changed') return;
+			libx.pax.triggerChange(mainJS);
+		});
+	}
+
+	console.log('-- Ready!')
+
+})();
---
+++ examples/src/main.js
@@ -0,0 +1,38 @@
+module.exports = (function(){
+	// Include libx.js essentials in the outputted file as global (=window) variable:
+	global._libx_avoidExtensions = true; // comment out if those extensions interfere with other libraries and uncomment the 'libx.extensions.apply*' commands bellow
+	global.libx = require('libx.js/bundles/browser.essentials');
+	// libx.extensions.applyStringExtensions();
+	// libx.extensions.applyDateExtensions();
+	// libx.extensions.applyArrayExtensions();
+	
+	// libx.log.isDebug = true;
+
+	// Register general dependencies:
+	require('libx.js/modules/network');
+	require('libx.js/modules/appEvents');
+	require('libx.js/modules/rxjs');
+
+	// Register modules into DI:
+	// window.myModule = new (require('./modules/myModule').default)();
+	// libx.di.register('myModule', window.myModule);
+
+
+	window.onload = (() => {
+		// libx.di.register('myModule2', new (require('./modules/myModule2').default)(args));
+	});
+
+	// Example how to require DI for specific modules (note: if one of the dependencies is not yet available it'll become pending func):
+	libx.di.require((log, network, activityLog, myModule) => {
+		log.isDebug = false;
+		log.debug('net: ', network, activityLog)
+	});
+
+	// Example how to require DI module (not recommended, use `require`. This will fail because 'log' is not immediately available):
+	// libx.di.get('log').verbose('Hello');
+
+	// Say Hey on startup to prove we're there
+	libx.log.v('main.js is ready')
+
+	return global.libx;
+})();
